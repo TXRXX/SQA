66a845acc1bb62b6318a6544216477e657a4f429
TXRXX <toro2544@gmail.com>
2022-07-05 18:53:52 +0700

    (sqa) : init lab2

---
+++ lab2/src/lab2/testing.java
@@ -0,0 +1,14 @@
+package lab2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testing {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
